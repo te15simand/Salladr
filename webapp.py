d6404dfcb3eaeb8f4e46df76cb73cb41face2bd5
Simon Andersson <te15simand@falufri.se>
2017-01-31 08:54:18 +0100

    Initial commit

---
+++ webapp.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask("salladr")
+
+visits = 0
+
+@app.route("/")
+def home():
+	global visits
+	visits += 1
+	return render_template("index.html", visits = visits,
+		username = "Simon")
+
+
+
+@app.route("/about/")
+def about():
+	return render_template("sallad.html")
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
