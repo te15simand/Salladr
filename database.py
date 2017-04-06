2ff0a1aa42e414fbdacb40fc23844eb2941e3859
Simon Andersson <te15simand@falufri.se>
2017-04-06 15:34:11 +0200

    Fix PostgreSql

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from peewee import PostgresqlDatabase, Model, CharField
 from playhouse.db_url import connect
 
-DATABASE_URL = os.environ.get("postgres://ujbaykmdqeqpux:bcc0efd6ab9b799a34d100d512f4d67a667d7ad7c73015544e34d3cf02bcc592@ec2-79-125-13-42.eu-west-1.compute.amazonaws.com:5432/dep90au2ofgb0")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 if DATABASE_URL:
 	db = connect(DATABASE_URL)
 else:
