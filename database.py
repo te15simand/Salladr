af22449be905ab4fad972d6fcf12dc0c1d448d0e
Simon Andersson <te15simand@falufri.se>
2017-04-06 15:25:53 +0200

    Add DATABASE_URL

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
+import os
 from peewee import PostgresqlDatabase, Model, CharField
+from playhouse.db_url import connect
 
-DATABASE = "Salladr"
-
-db = PostgresqlDatabase(DATABASE, threadlocals=True)
+DATABASE_URL = os.environ.get("postgres://ujbaykmdqeqpux:bcc0efd6ab9b799a34d100d512f4d67a667d7ad7c73015544e34d3cf02bcc592@ec2-79-125-13-42.eu-west-1.compute.amazonaws.com:5432/dep90au2ofgb0")
+if DATABASE_URL:
+	db = connect(DATABASE_URL)
+else:
+	DATABASE = "Salladr"
+	db = PostgresqlDatabase(DATABASE, threadlocals=True)
 
 class BaseModel(Model):
 	class Meta:
