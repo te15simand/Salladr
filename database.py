59f8d9de8f7cf598700764cc5205bd80c6fbada0
Simon Andersson <te15simand@falufri.se>
2017-05-04 14:10:06 +0200

    Cookie uppgift 1

--- database.py
+++ database.py
@@ -2,11 +2,12 @@ import os
 from peewee import PostgresqlDatabase, Model, CharField
 from playhouse.db_url import connect
 
+
 DATABASE_URL = os.environ.get("DATABASE_URL")
 if DATABASE_URL:
 	db = connect(DATABASE_URL)
 else:
-	DATABASE = "Salladr"
+	DATABASE = "salladr"
 	db = PostgresqlDatabase(DATABASE, threadlocals=True)
 
 class BaseModel(Model):
--- webapp.py
+++ webapp.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, session
 from forms import MyForm
 from database import db
-
+import os
 
 app = Flask("salladr")
 app.config["WTF_CSRF_ENABLED"] = False
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "insecure dev key")
 
 @app.before_request
 def before_requrst():
@@ -14,6 +15,29 @@ def before_requrst():
 def after_request(response):
 	db.close()
 	return response
+
+
+@app.route("/track/cookie") 
+def track_cookie():
+	visits = int(request.cookies.get("number_of_visits", 0))
+	if visits == 0:
+		text = "Welcome! This is your first visit!"
+		visits += 1
+	elif visits >= 1:
+		visits += 1
+		text = "Welcome back!"
+
+	resp = make_response(text)
+	resp.set_cookie("number_of_visits", str(visits))
+	return resp
+
+@app.route("/track/session")
+def track_session():
+	visits = int(session.get("number_of_visits", 0))
+	visits += 1
+	session["number_of_visits"] = visits
+	text = "Welcome! This is visit number: {}".format(visits)
+	return text
 
 @app.route("/")
 def home():
